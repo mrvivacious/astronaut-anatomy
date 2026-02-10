586fdd0a466be0bf662fb6e6550339512c2a805e
Vivek Bhookya <v.bhookya98@gmail.com>
2026-02-10 01:57:55 -0600

    getTotalNumberOfAstronauts()

--- data.js
+++ data.js
@@ -589,3 +589,16 @@ function getGroupNames() {
 function getGroup(groupName) {
   return nasa.astronaut_groups[groupName];
 }
+
+function getTotalNumberOfAstronauts() {
+  let total = 0;
+  for (let groupName in nasa.astronaut_groups) {
+    total += nasa.astronaut_groups[groupName].astronauts.length;
+  }
+  return total;
+}
+
+// compute and display ratio of astronauts with military experience
+// compute and display astronauts by military branch + unaffiliated
+// compute and display statistics related to degrees, ie. (20 PhDs, 13 Masters,...)
+// + only count one degree per person, the highest degree earned
--- loader.js
+++ loader.js
@@ -1,20 +1,4 @@
 // loader.js
 
-// etc
-
-let p = document.getElementById("data");
-let output = '';
-let astronauts = nasa.astronaut_groups.group_23.astronauts
-
-for (let i = 0; astronauts[i]; i++) {
-  let current = astronauts[i];
-  let name = current.name;
-  let year = "2023";
-
-  let str = `<h3>${name}</h3>`
-  str += `<p>Selected in ${year}</p>`
-  
-  output += str;
-}
-
-p.innerHTML = output;
+let astronautCountElement = document.getElementById('astronaut_count');
+astronautCountElement.innerText = getTotalNumberOfAstronauts();
